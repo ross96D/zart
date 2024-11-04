50cd995fdf7ab5e66ceef58bddfa6128da11ebf0
ross96D <abelaross96@gmail.com>
2024-11-04 18:09:02 -0500

    fix build

--- build.zig
+++ build.zig
@@ -8,7 +8,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const mod = b.addModule("zart", .{
-        .root_source_file = "src/zart.zig",
+        .root_source_file = b.path("src/zart.zig"),
         .optimize = optimize,
         .target = target,
     });
