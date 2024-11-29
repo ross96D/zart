8d778457c058a7032e6db15c592afbfae58988a2
ross96D <abelaross96@gmail.com>
2024-11-29 00:07:47 -0500

    implementing pool_allocator

--- src/pool_allocator.zig
+++ src/pool_allocator.zig
@@ -21,15 +21,20 @@ const Error = Allocator.Error;
 const assert = std.debug.assert;
 
 pub fn Pool(T: type) type {
-    const SIZE = @sizeOf(T);
-    _ = SIZE;
+    const ITEM_SIZE = @sizeOf(T);
     const BLOCK_SIZE = 512;
     return struct {
         const Self = @This();
 
         const Block = struct {
-            block: [BLOCK_SIZE]T = undefined,
-            length: usize,
+            fn order(b1: *Block, b2: *Block) std.math.Order {
+                const ptr1 = @intFromPtr(b1);
+                const ptr2 = @intFromPtr(b2);
+                return std.math.order(ptr1, ptr2);
+            }
+
+            values: [BLOCK_SIZE]T = undefined,
+            lenght: usize,
 
             inline fn reset(self: *Block) void {
                 self.lenght = 0;
@@ -39,68 +44,166 @@ pub fn Pool(T: type) type {
                 return self.lenght == BLOCK_SIZE;
             }
 
-            inline fn is_empty(self: *const Block) bool {
-                // return std.mem.eql(bool, &self.active, &all_inactive);
-            }
-
             fn next(self: *Block) *T {
                 assert(!self.is_full());
-                // TODO
-                const ret = &self.block[self.lenght];
-                self.active[self.lenght] = true;
+                const ret = &self.values[self.lenght];
                 self.lenght += 1;
                 return ret;
             }
         };
 
         const BlockReusableItems = struct {
-            ptr: *Block,
-        };
+            fn order(b1: BlockReusableItems, b2: BlockReusableItems) std.math.Order {
+                const ptr1 = @intFromPtr(b1.ptr);
+                const ptr2 = @intFromPtr(b2.ptr);
+                return std.math.order(ptr1, ptr2);
+            }
 
-        const FreeList = struct {
-            // sorted list of pointrs to block
-            blocks: SortedList(usize),
-            inline fn init(allocator: Allocator) FreeList {
-                return .{ .blocks = SortedList(usize).init(allocator) };
+            /// pointer to the allocated Block
+            ptr: *Block,
+            /// List of indexes that had been freed
+            ///
+            /// This could be improved if we take into account that the
+            /// list max len can only be BLOCK_SIZE
+            reusable: std.ArrayList(usize),
+
+            inline fn init(allocator: Allocator, block: *Block) BlockReusableItems {
+                return .{
+                    .reusable = std.ArrayList(usize).init(allocator),
+                    .ptr = block,
+                };
+            }
+
+            inline fn deinit(self: BlockReusableItems) void {
+                self.reusable.deinit();
+            }
+
+            fn reuse(self: *BlockReusableItems, allocated_list: *const AllocatedList) ?*T {
+                if (self.reusable.items.len == 0) {
+                    return null;
+                }
+                const index_item = self.reusable.pop();
+                const index_block = allocated_list.index_of(self.ptr).?;
+                const block = allocated_list.slice()[index_block];
+                return &block.values[index_item];
+            }
+
+            fn can_be_free(self: *BlockReusableItems) bool {
+                return self.ptr.lenght == self.reusable.items.len;
             }
         };
 
+        const ReusableList = struct {
+            const List = SortedList(BlockReusableItems, BlockReusableItems.order);
+
+            /// track all the allocated blocks and the reusable elements
+            blocks: List,
+            inline fn init(allocator: Allocator) ReusableList {
+                return .{
+                    .blocks = List.init(allocator),
+                };
+            }
+
+            fn reuse(self: *ReusableList, allocated_list: *const AllocatedList) ?*T {
+                for (self.blocks.slice()) |*block| {
+                    if (block.reuse(allocated_list)) |elem| {
+                        return elem;
+                    }
+                }
+                return null;
+            }
+
+            inline fn get(self: ReusableList, block: *Block) ?BlockReusableItems {
+                const index = self.blocks.index_of(.{
+                    .ptr = block,
+                    .reusable = undefined,
+                }) orelse return null;
+
+                return self.blocks.values.items[index];
+            }
+        };
+
+        const AllocatedList = SortedList(*Block, Block.order);
+
         allocator: Allocator,
         // we should write this our self to tune the grow scale... TODO
-        allocated_list: SortedList(*Block),
+        allocated_list: AllocatedList,
         // list containing the index of the free items on allocated_list
-        free_list: FreeList,
+        reusable_list: ReusableList,
+
+        /// Cache the last allocated for fast create
+        last_allocated: *Block = undefined,
 
         pub fn init(allocator: Allocator) Self {
             return Self{
                 .allocator = allocator,
-                .allocated_list = std.ArrayList(Block).init(allocator),
-                .free_list = unreachable,
+                .allocated_list = AllocatedList.init(allocator),
+                .reusable_list = ReusableList.init(allocator),
             };
         }
 
+        fn new_block(self: *Self) Error!*Block {
+            var block = try self.allocator.create(Block);
+            block.reset();
+            self.last_allocated = block;
+            try self.allocated_list.append(block);
+            try self.reusable_list.blocks.append(BlockReusableItems.init(self.allocator, block));
+            return block;
+        }
+
         pub fn create(self: *Self) Error!*T {
-            // TODO check free list
             if (self.allocated_list.len() == 0) {
-                var block = try self.allocator.create(Block);
-                block.reset();
-                self.allocated_list.append(block);
+                var block = try self.new_block();
                 return block.next();
             }
-            // for (self.)
-
-            // try self.allocated_list.addOne();
-            // last_block = &self.allocated_list.items[self.allocated_list.items.len - 1];
+            if (self.reusable_list.reuse(&self.allocated_list)) |elem| {
+                return elem;
+            }
+            if (!self.last_allocated.is_full()) {
+                return self.last_allocated.next();
+            }
+            var block = try self.new_block();
+            return block.next();
         }
 
         pub fn destroy(self: *Self, item: *T) Error!void {
-            _ = self;
-            _ = item;
+            for (self.allocated_list.slice()) |block| {
+                const ptr = @intFromPtr(item);
+                const start = @intFromPtr(block);
+                const size = BLOCK_SIZE * ITEM_SIZE;
+                // this should always be valid because the list is sorted by ptrs on ascending order
+                assert(ptr >= start);
+
+                const offset = ptr - start;
+                if (offset >= size) {
+                    continue;
+                }
+                const index = @divExact(offset, ITEM_SIZE);
+                var reusable_block = self.reusable_list.get(block).?;
+                try reusable_block.reusable.append(index);
+
+                if (reusable_block.can_be_free()) {
+                    self.reusable_list.blocks.remove(reusable_block);
+                    self.allocated_list.remove(reusable_block.ptr);
+
+                    if (reusable_block.ptr == self.last_allocated and self.allocated_list.len() != 0) {
+                        // we only set a valid element to avoid an invalid address access
+                        // but the element as long as is valid is not important
+                        // because every block on the list is full and will trigger
+                        // the creation of a new block
+                        self.last_allocated = self.allocated_list.values.items[0];
+                    }
+
+                    self.allocator.destroy(reusable_block.ptr);
+                    reusable_block.deinit();
+                }
+                break;
+            }
         }
     };
 }
 
-fn SortedList(T: type) type {
+fn SortedList(T: type, compare: fn (T, T) std.math.Order) type {
     return struct {
         const Self = @This();
 
@@ -109,6 +212,7 @@ fn SortedList(T: type) type {
         pub inline fn init(allocator: Allocator) Self {
             return .{ .values = std.ArrayList(T).init(allocator) };
         }
+
         pub inline fn deinit(self: Self) void {
             self.values.deinit();
         }
@@ -159,17 +263,15 @@ fn SortedList(T: type) type {
                 if (pos == 0) {
                     break;
                 }
-                if (v < array[pos]) {
-                    high = pos - 1;
-                } else if (v > array[pos]) {
-                    low = pos;
-                } else {
-                    return pos;
+                switch (compare(v, array[pos])) {
+                    .eq => return pos,
+                    .lt => high = pos - 1,
+                    .gt => low = pos,
                 }
             }
             pos = low;
 
-            if (v > array[pos]) {
+            if (compare(v, array[pos]) == .gt) {
                 return pos + 1;
             }
             return pos;
@@ -182,12 +284,10 @@ fn SortedList(T: type) type {
             var high: usize = length;
             while (low < high) {
                 const i: usize = (low + high) / 2;
-                if (v > array[i]) {
-                    low = i + 1;
-                } else if (v < array[i]) {
-                    high = i;
-                } else {
-                    return i;
+                switch (compare(v, array[i])) {
+                    .eq => return i,
+                    .lt => high = i,
+                    .gt => low = i + 1,
                 }
             }
             return null;
@@ -195,9 +295,25 @@ fn SortedList(T: type) type {
     };
 }
 
+test "TODO" {
+    std.testing.refAllDecls(Pool(usize));
+}
+
+const ImplOrder = struct {
+    fn order_u32(lhs: u32, rhs: u32) std.math.Order {
+        return std.math.order(lhs, rhs);
+    }
+    fn order_usize(lhs: usize, rhs: usize) std.math.Order {
+        return std.math.order(lhs, rhs);
+    }
+    fn order_i32(lhs: i32, rhs: i32) std.math.Order {
+        return std.math.order(lhs, rhs);
+    }
+};
+
 test SortedList {
     { // basic
-        var list = SortedList(usize).init(std.testing.allocator);
+        var list = SortedList(usize, ImplOrder.order_usize).init(std.testing.allocator);
         defer list.deinit();
 
         try list.append(3);
@@ -230,7 +346,7 @@ test SortedList {
 
     { // fuzzy
         { // append
-            var list = SortedList(usize).init(std.testing.allocator);
+            var list = SortedList(usize, ImplOrder.order_usize).init(std.testing.allocator);
             defer list.deinit();
 
             const seed: u64 = 1732828618; // @intCast(std.time.timestamp());
@@ -256,7 +372,7 @@ test SortedList {
         }
 
         { // search
-            var list = SortedList(usize).init(std.testing.allocator);
+            var list = SortedList(usize, ImplOrder.order_usize).init(std.testing.allocator);
             defer list.deinit();
 
             const seed: u64 = 1732828618; // @intCast(std.time.timestamp());
